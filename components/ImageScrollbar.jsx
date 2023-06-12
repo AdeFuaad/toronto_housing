4d4140ac362f8c7b8583f51b48b797e01dde8239
AdeFuaad <adesholafuaad@gmail.com>
2023-06-12 18:52:31 -0400

    UI corrections

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -2,10 +2,8 @@ import { useContext } from "react";
 import Image from "next/image";
 import { Box, Icon, Flex, Text } from "@chakra-ui/react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
 
   return (
     <Flex
@@ -21,27 +19,11 @@ const LeftArrow = () => {
   );
 };
 
-// const RightArrow = () => {
-//   const { scrollNext } = useContext(VisibilityContext);
-
-//   return (
-//     <Flex justifyContent="center" alignItems="center" marginLeft="1">
-//       <Icon
-//         as={FaArrowAltCircleRight}
-//         onClick={() => scrollNext()}
-//         fontSize="2xl"
-//         cursor="pointer"
-//         d={["none", "none", "none", "block"]}
-//       />
-//     </Flex>
-//   );
-// };
 
 export default function ImageScrollbar({ data }) {
   return (
     <ScrollMenu
       LeftArrow={LeftArrow}
-      // RightArrow={RightArrow}
       style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
